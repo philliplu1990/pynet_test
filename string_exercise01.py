d07d78ebdc5d7d2b1de32baf9647371ce59aade7
Phil Lu <phillip@uber.com>
2017-03-20 14:12:24 -0700

    Add string exercise01

---
+++ string_exercise01.py
@@ -0,0 +1,8 @@
+name1 = 'Jarrod Phelps'
+name2 = 'Phil Lu'
+name3 = 'Mike Meeker'
+
+print "{:>30}{:>30}{:>30}".format(name1, name2, name3)
+
+name4 = raw_input('Print your name please:')
+print name4
