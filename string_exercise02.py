a1da8e1bd5dbc3d90b6738b76c07cfa211e6f1de
Phil Lu <phillip@uber.com>
2017-03-20 14:25:43 -0700

    Modify ex2

--- string_exercise02.py
+++ string_exercise02.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
 ip_address = raw_input('Enter IP Address: ')
 octets = ip_address.split('.')
-print "{:<12}{:<12}{:<12}{:<12}".format(octets[0],octets[1],octets[2],octets[3])
+print "{:<12}{:<12}{:<12}{:<12}".format(*octets)
 
